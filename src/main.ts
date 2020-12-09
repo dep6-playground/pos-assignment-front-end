015a1c5de594b4d3ee00dd24587ee570d7e0567e
Ranjith Suranga <d.m.ranjith.sura@gmail.com>
2020-12-09 15:56:57 +0530

    Add admin-lte js deps

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,9 @@
 
 import './app/app.module';
 import style from './style.scss';
+import '../node_modules/admin-lte/plugins/bootstrap/js/bootstrap.bundle.min.js';
+import '../node_modules/admin-lte/plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js';
+import '../node_modules/admin-lte/dist/js/adminlte.js';
 
 var html = '<style>' + style + '</style>';
 $("head").append(html);
